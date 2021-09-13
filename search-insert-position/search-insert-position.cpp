2d55c54d3c2b0ec8e5e6fe0f22d24521cef9b0d2
Jack Cho <jack06215@users.noreply.github.com>
2021-09-13 23:55:54 +0800

    Time: 0 ms (100.00%), Space: 9.7 MB (55.36%) - LeetHub

---
+++ search-insert-position/search-insert-position.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size();
+        
+        while (left < right) {
+            // increment step by middle point
+            int mid = left + (right - left) / 2;
+            
+            // we found the answer!
+            if (nums[mid] == target) {
+                return mid;
+            }
+            
+            // too big, shift the high bound
+            else if (nums[mid] >= target) {
+                right = mid;
+            }
+            // too small, sift the low bound
+            else {
+                left = mid + 1; 
+            }
+        }
+        return left;
+    }
+};
