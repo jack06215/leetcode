e89c0f5a2a4e8217300031707297b5d6cedc71fc
Jack Cho <jack06215@users.noreply.github.com>
2021-09-14 00:13:02 +0800

    Time: 4 ms (73.96%), Space: 9.6 MB (5.39%) - LeetHub

---
+++ search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty()) return false;
+        
+        int n_cols = matrix[0].size();
+        int left = 0;
+        int right = matrix.size() * n_cols - 1;
+        
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            auto [i, j] = to_2d_coordinate(mid, n_cols);
+            
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            else if (matrix[i][j] > target) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+private:
+    pair<int, int> to_2d_coordinate(int index, int stride) {
+        return { index / stride, index % stride };
+    }
+};
