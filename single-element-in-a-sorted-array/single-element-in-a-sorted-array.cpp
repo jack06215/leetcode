b1d8444afde1acdc8feeb6a4733476792382f989
Jack Cho <jack06215@users.noreply.github.com>
2021-09-15 00:16:40 +0800

    Time: 16 ms (8.11%), Space: 11.1 MB (39.96%) - LeetHub

--- single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -6,7 +6,8 @@ public:
         
         while( left < right ) {
             int mid = left + (right - left) / 2;
-            if( nums[mid] == nums[mid ^ 1] )
+            cout << (mid ^ 1) << endl;
+            if(nums[mid] == nums[mid ^ 1])
                 left = mid + 1;
             else
                 right = mid;
