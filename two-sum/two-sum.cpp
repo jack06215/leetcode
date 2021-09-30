f16a7eb28562579463c3695f436df369bcac03a8
Jack Cho <jack06215@users.noreply.github.com>
2021-09-30 23:58:16 +0800

    Time: 12 ms (76.49%), Space: 10.9 MB (40.60%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,14 +1,34 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map <int, int> m;
+        vector<int> result;
+        
+        vector<pair<int, int>> lookup;
         for (int i = 0; i < nums.size(); ++i) {
-            int val = target - nums[i];
-            if (m.find(val) != m.end()) {
-                return  { m[val], i };
-            }
-            m[nums[i]] = i;
+            lookup.push_back({nums[i], i});
         }
-        return { };
+        sort(lookup.begin(), lookup.end());
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int val = lookup[left].first + lookup[right].first;
+            if (val == target) {
+                result.push_back(lookup[left].second);
+                result.push_back(lookup[right].second);
+                break;
+            }
+            else if (val > target) {
+                right--;
+            }
+            else {
+                left++;
+            }
+        }
+        return result;
     }
 };
