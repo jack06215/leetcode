da3686c3ff88732ed7e547c40f82024ef342062b
Jack Cho <jack06215@users.noreply.github.com>
2021-09-30 22:37:20 +0800

    Time: 4 ms (99.53%), Space: 10.8 MB (53.73%) - LeetHub

---
+++ two-sum/two-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map <int, int> m;
+        for (int i = 0; i < nums.size(); ++i) {
+            int val = target - nums[i];
+            if (m.find(val) != m.end()) {
+                return  { m[val], i };
+            }
+            m[nums[i]] = i;
+        }
+        return { };
+    }
+};
