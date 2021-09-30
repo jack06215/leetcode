b8b3df169683d3eb75c0dff66bf13891412e8123
Jack Cho <jack06215@users.noreply.github.com>
2021-09-30 23:34:49 +0800

    Time: 8 ms (49.70%), Space: 9.9 MB (91.86%) - LeetHub

--- intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,3 +1,8 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
@@ -5,8 +10,8 @@ public:
         sort(nums2.begin(), nums2.end());
         
         vector<int> res;
-        for (int& num1: nums1) {
-            int val = binarySearch(nums2, num1, 0, nums2.size() - 1);
+        for (int& target: nums1) {
+            int val = binarySearch(nums2, target);
             if (val != -1) {
                 res.emplace_back(val);
             }
@@ -14,7 +19,12 @@ public:
         return res;
     }
 private:
-    int binarySearch(vector<int>& arr, int target, int left, int right) {
+    int binarySearch(vector<int>& arr, int target) {
+        if (arr.empty()) {
+            return -1;
+        }
+        int left = 0;
+        int right = arr.size() - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (arr[mid] == target) {
