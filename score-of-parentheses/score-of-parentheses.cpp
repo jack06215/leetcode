af8619526a2454d5df0fbed0ba11a994c118258b
Jack Cho <jack06215@users.noreply.github.com>
2021-10-17 12:34:28 +0800

    Time: 0 ms (100.00%), Space: 6.1 MB (82.64%) - LeetHub

---
+++ score-of-parentheses/score-of-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack<int> stack;
+        int res = 0;
+        for (auto c : s) {
+            if (c == '(') {
+                stack.push(res);
+                res = 0;
+            }
+            else {
+                int prev = stack.top();
+                stack.pop();
+                res += prev + max(res, 1);
+            }
+        }
+        return res;
+    }
+};
