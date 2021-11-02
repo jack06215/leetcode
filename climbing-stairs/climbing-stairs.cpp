5c38be874784fb2f92c0939fd7f28627e5ae41cf
Jack Cho <jack06215@users.noreply.github.com>
2021-11-02 22:23:02 +0800

    Time: 0 ms (100.00%), Space: 6.3 MB (13.59%) - LeetHub

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -1,26 +1,25 @@
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
-    int f(int n, int *arr)
-    {
-        if (n == 0 || n == 1) return 1;
-        if (arr[n] != 0) return arr[n];
-        else{
-          arr[n] = f(n - 1, arr) + f(n - 2, arr);
-          return arr[n];
+    int solve(int n, int* arr) {
+        if (arr[n] != -1) {
+            return arr[n];
         }
+        arr[n] = solve(n - 1, arr) + solve(n - 2, arr);
+        return arr[n];
     }
-
+    
     int climbStairs(int n) {
-        int *p = (int *)malloc(sizeof(int) * (n + 1));
-        int res, i;
-
-        if (n == 0 || n == 1) p[n] = 1;  //Base condition
-
-        for (i = 2; i <= n; i++) p[i] = 0; //For memoization, defaulting all values to 0
-
-        res = f(n, p);
-        free(p);
-
-        return res;
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        memo.resize(n + 1, -1);
+        memo[0] = 1, memo[1] = 1;
+        return solve(n, &memo[0]);
     }
+private:
+    vector<int> memo;
 };
