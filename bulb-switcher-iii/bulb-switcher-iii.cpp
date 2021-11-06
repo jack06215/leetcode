af9662f7fcdc23bdf8fde0590097e42e352483ee
Jack Cho <jack06215@users.noreply.github.com>
2021-11-06 23:24:36 +0800

    Time: 84 ms (35.53%), Space: 38.4 MB (91.71%) - LeetHub

---
+++ bulb-switcher-iii/bulb-switcher-iii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numTimesAllBlue(vector<int>& light) {
+        int max_bulb = 0;
+        int moment = 0;
+        int size = light.size();
+        
+        for (int i = 0; i < size; i++) {
+            max_bulb = max(max_bulb, light[i]);
+            if (max_bulb == i + 1) {
+                moment++;
+            }
+        }
+        return moment;
+    }
+};
