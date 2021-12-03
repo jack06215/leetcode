57530eb37e0f5c9d2c7eb0e5a887032b1f485a2c
Jack Cho <jack06215@users.noreply.github.com>
2021-12-03 23:30:25 +0800

    Time: 4 ms (97.59%), Space: 10.1 MB (56.98%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        return quickSelect(nums, 0, nums.size() - 1, k);
-    }
-    
-    int quickSelect(vector<int>& arr, int start, int end, int k) {
-        /**
-         * Use pivotIndex as the seperater. If k is smaller than the length of the 
-         * right side of the array, the target is in the right side of the array. 
-         * If k is larger, the target is in the left side of the array. 
-         */
-        int pivotIndex = partition(arr, start, end);
-        if (k < arr.size() - pivotIndex) {
-            return quickSelect(arr, pivotIndex + 1, end, k);
-        } else if (k > arr.size() - pivotIndex) {
-            return quickSelect(arr, start, pivotIndex - 1, k);
-        }
-        // pivotIndex is the index of the target if k equals to the length of the 
-        // right side of the array.
-        return arr[pivotIndex];
-    };
-    
-    int partition(vector<int>& array, int left, int right) {
-        int pivot = array[right];
-        int i = left -1;
-        for (int j = left; j < right; j++) {
-            if (array[j] < pivot) {
-                i++;
-                swap(array[i], array[j]);
+        struct cmp {
+            bool operator()(const int& left, const int& right) {
+                return left > right;
+            }
+        };
+        priority_queue<int, vector<int>, cmp> q;
+        for (int i = 0; i < nums.size(); i++) {
+            q.push(nums[i]);
+            if (q.size() > k) {
+                q.pop();
             }
         }
-        i++;
-        swap(array[i], array[right]);
-        return i;
+        return q.top();
     }
 };
