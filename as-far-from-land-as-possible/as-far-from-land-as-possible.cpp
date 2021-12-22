3c1dddc2f25704ab02c19ad2aca49229857fc925
Jack Cho <jack06215@users.noreply.github.com>
2021-12-22 22:04:03 +0800

    Time: 68 ms (48.13%), Space: 27.4 MB (22.56%) - LeetHub

---
+++ as-far-from-land-as-possible/as-far-from-land-as-possible.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        queue<pair<int, int>> bfs, next;
+        int step = 0;
+
+        const int n_rows = grid.size();
+        const int n_cols = grid[0].size();
+
+        for (int i = 0; i < n_rows; i++) {
+            for (int j = 0; j < n_cols; j++) {
+                if (grid[i][j] == 1) {
+                    bfs.push({ i + 1, j });
+                    bfs.push({ i - 1, j });
+                    bfs.push({ i, j + 1 });
+                    bfs.push({ i, j - 1 });
+                }
+            }
+        }
+
+        while (!bfs.empty()) {
+            step++;
+            while (!bfs.empty()) {
+                auto [i, j] = bfs.front();
+                bfs.pop();
+                if (i >= 0 && j >= 0 && i < n_rows && j < n_cols && grid[i][j] == 0) {
+                    grid[i][j] = step;
+                    next.push({ i + 1, j });
+                    next.push({ i - 1, j });
+                    next.push({ i, j + 1 });
+                    next.push({ i, j - 1 });
+                }
+            }
+            swap(next, bfs);
+        }
+        return step == 1 ? -1 : step - 1;
+    }
+};
