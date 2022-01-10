d60b682ac7c9454691a29f46c72658bede48b078
Jack Cho <jack06215@users.noreply.github.com>
2022-01-10 10:30:15 +0800

    Time: 3 ms, Memory: 5.9 MB - LeetHub

--- sqrtx/sqrtx.cpp
+++ sqrtx/sqrtx.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 0) return 0;
-        else if (x < 3) return 1;
+        if (x == 0) {
+            return 0;
+        }
+        else if (x <= 3) {
+            return 1;
+        }
         
-        uint64_t left = 0;
+        uint64_t left = 1;
         uint64_t right = x;
         
-        uint64_t result;
         while (left < right) {
-            uint64_t mid = (left + right) / 2;
+            uint64_t mid = left + (right - left) / 2;
             uint64_t squared = mid * mid;
-            
             if (squared == x) {
                 return mid;
             }
-            else if (squared >= x) {
-                
+            else if (squared > x) {
                 right = mid;
             }
             else {
                 left = mid + 1;
-                result = mid;
             }
         }
-        return result;
+        return left - 1;
     }
 };
