58033014fff8235c80223a336a66869cca852e8f
Jack Cho <jack06215@users.noreply.github.com>
2022-01-10 17:42:22 +0800

    Time: 0 ms, Memory: 5.7 MB - LeetHub

---
+++ valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int left = 1;
+        int right = num;
+        while (left <= right) {
+            uint64_t mid = left + (right - left) / 2;
+            uint64_t squared = mid * mid;
+            if (squared == num) {
+                return true;
+            }
+            else if (squared >= num) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+};
