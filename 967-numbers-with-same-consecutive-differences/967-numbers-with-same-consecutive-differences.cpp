af4155c2eccf1c974e04b47e6025dba12047eb9a
Jack Cho <jack06215@users.noreply.github.com>
2022-01-15 00:06:24 +0800

    Time: 5 ms (24.89%), Space: 7.2 MB (85.84%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,37 +1,33 @@
 class Solution {
-    vector<int> res;
+    const int MAX_N = 9;
+    const int MAX_K = 9;
 public:
-    void backtrack(int& n, int pos, int& k, int num) {
-        if (pos == n) {
-            res.push_back(num);
-            return;
-        }
-        if (pos > n) {
-            return;
-        }
-        int last_digit = num % 10;
-        int next_digit;
-
-        next_digit = last_digit + k;
-        if (next_digit <= 9) {
-            int temp = num;
-            num = 10 * num + next_digit;
-            backtrack(n, pos + 1, k, num);
-            num = temp;
-        }
-
-        next_digit = last_digit - k;
-        if (k != 0 && next_digit >= 0) {
-            int temp = num;
-            num = 10 * num + next_digit;
-            backtrack(n, pos + 1, k, num);
-            num = temp;
-        }
-    }
-
     vector<int> numsSameConsecDiff(int n, int k) {
-        for (int i = 1; i <= 9; ++i) {
-            backtrack(n, 1, k, i);
+        queue<int> q;
+        vector<int> res;
+        for (int i = 1; i <= MAX_N; i++) {
+            q.push(i);
+        }
+        for (int i = 1; i < n; i++) {
+            int size = q.size();
+            for (int i = 0;i < size;i++) {
+                int num = q.front();
+                q.pop();
+                int last_digit = num % 10;
+                if (last_digit + k <= MAX_K) {
+                    q.push(num * 10 + last_digit + k);
+                }
+                if (last_digit - k >= 0 && k != 0) {
+                    q.push(num * 10 + last_digit - k);
+                }
+            }
+        }
+        if (n == 1) {
+            res.push_back(0);
+        }
+        while (!q.empty()) {
+            res.push_back(q.front());
+            q.pop();
         }
         return res;
     }
