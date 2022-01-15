dac812c833fdfc653fdc019de97ae67bf7e4a8c2
Jack Cho <jack06215@users.noreply.github.com>
2022-01-15 18:27:40 +0800

    Time: 8 ms (55.55%), Space: 9.4 MB (57.51%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
-    string simplifyPath(string path) {
-        string res;
-        istringstream tokenizer(path);
-
-        vector<string> dir;
-        string token;
-        while (getline(tokenizer, token, '/')) {
-            // no change
-            if (token == "." || token == "") {
-                continue;
-            }
-            // back to parent directory, skip if it's already root directory
-            else if (token == "..") {
-                if (!dir.empty()) {
-                    dir.pop_back();
-                }
-            }
-            // go inner directory
-            else {
-                dir.push_back(token);
-            }
-        }
-        // root directory
-        if (dir.empty()) {
-            return "/";
-        }
-
-        // make conanical directory
-        for (const auto& it : dir) {
-            res += '/' + it;
-        }
-        return res;
+string simplifyPath(string path) {
+    string res, tmp;
+    vector<string> stk;
+    stringstream ss(path);
+    while(getline(ss,tmp,'/')) {
+        if (tmp == "" or tmp == ".") continue;
+        if (tmp == ".." and !stk.empty()) stk.pop_back();
+        else if (tmp != "..") stk.push_back(tmp);
     }
+    for(auto str : stk) res += "/"+str;
+    return res.empty() ? "/" : res;
+}
 };
