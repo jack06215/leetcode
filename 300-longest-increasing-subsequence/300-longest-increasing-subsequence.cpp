dbd065fb82cd5de7d707735262f202e6acc9e9ff
Jack Cho <jack06215@users.noreply.github.com>
2022-01-15 13:11:26 +0800

    Time: 8 ms (90.59%), Space: 10.5 MB (63.42%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -3,15 +3,17 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         vector<int> result;
         for (int i = 0; i < nums.size(); i++) {
-            int cur_size = result.size();
+            int n = result.size();
             int left = 0;
-            int right = cur_size - 1;
+            int right = n - 1;
+            // keep pushing increasing subsequence
             if (result.empty() || result[right] < nums[i]) {
                 result.push_back(nums[i]);
             }
+            // 
             else {
                 while (left < right) {
-                    int mid = (left + right) / 2;
+                    int mid = left + (right - left) / 2;
                     if (result[mid] >= nums[i]) {
                         right = mid;
                     }
