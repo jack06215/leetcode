a7e1b37772781865699ff7352aa70059cde7f557
Jack Cho <jack06215@users.noreply.github.com>
2022-01-15 17:58:05 +0800

    Time: 7 ms (6.68%), Space: 6.9 MB (33.04%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> pascal;
 
-    vector<int> getVector(vector<int> vec, vector<int> preVec, int i) {
+    vector<int> getVector(vector<int>& vec, vector<int>& preVec, int i) {
         // i starts from 2 and goes till the previous vector length of the triangle
         if (i == preVec.size()) {
             return vec;
@@ -10,18 +10,18 @@ public:
         vec[i] = preVec[i - 1] + preVec[i];
         return getVector(vec, preVec, ++i);
     }
-    vector<vector<int>> makeTriangle(int made, int n_rols) {
-        if (made == n_rols) {
+    vector<vector<int>> makeTriangle(int n, int n_rols) {
+        if (n == n_rols) {
             return pascal;
         }
         // initializing the first and last elements of the vector to be added
-        vector<int> vec(made + 1, 0);
-        vec[0] = 1;
-        vec[made] = 1;
+        vector<int> new_row(n + 1, 0);
+        new_row[0] = 1;
+        new_row[n] = 1;
 
         // gets the vector
-        pascal.push_back(getVector(vec, pascal[made - 1], 1));
-        return makeTriangle(++made, n_rols);
+        pascal.push_back(getVector(new_row, pascal[n - 1], 1));
+        return makeTriangle(++n, n_rols);
     }
     vector<vector<int>> generate(int numRows) {
         if (numRows == 0) {
