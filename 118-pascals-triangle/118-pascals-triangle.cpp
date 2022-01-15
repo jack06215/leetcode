6c076b46cd36683182937c196cf7911ffbebf897
Jack Cho <jack06215@users.noreply.github.com>
2022-01-15 18:07:50 +0800

    Time: 0 ms (100.00%), Space: 7.1 MB (33.04%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> pascal;
-
-    vector<int> getVector(vector<int>& vec, vector<int>& preVec, int i) {
-        // i starts from 2 and goes till the previous vector length of the triangle
-        if (i == preVec.size()) {
-            return vec;
-        }
-        vec[i] = preVec[i - 1] + preVec[i];
-        return getVector(vec, preVec, ++i);
-    }
-    vector<vector<int>> makeTriangle(int n, int n_rols) {
-        if (n == n_rols) {
-            return pascal;
-        }
-        // initializing the first and last elements of the vector to be added
-        vector<int> new_row(n + 1, 0);
-        new_row[0] = 1;
-        new_row[n] = 1;
-
-        // gets the vector
-        pascal.push_back(getVector(new_row, pascal[n - 1], 1));
-        return makeTriangle(++n, n_rols);
-    }
     vector<vector<int>> generate(int numRows) {
-        if (numRows == 0) {
-            return pascal;
+        ans.reserve(numRows);
+        helper(numRows);
+        return ans;
+    }
+
+private:
+    vector<vector<int>> ans;
+    vector<int> helper(int n) {
+        if (n == 1) {
+            ans.push_back({ { 1 } });
+            return ans[n - 1];
         }
-        else if (numRows == 1) {
-            return { {1} };
+        if (n == 2) {
+            helper(n - 1);
+            ans.push_back({ { 1, 1 } });
+            return ans[n - 1];
         }
-        else if (numRows == 2) {
-            return { {1},{1,1} };
+        vector<int> prev = helper(n - 1);
+        vector<int> temp(n, 1);
+        for (int i = 1; i < n - 1; i++) {
+            temp[i] = prev[i] + prev[i - 1];
         }
-        pascal = { {1},{1,1} };   // Now that the triangle definitely has more than 2 rows
-        return makeTriangle(2, numRows);
+        ans.push_back(temp);
+        return temp;
     }
 };
