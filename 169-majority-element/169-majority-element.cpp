8b2375eb154ed80f356b4f39f84eb14c62a5edca
Jack Cho <jack06215@users.noreply.github.com>
2022-01-15 23:14:19 +0800

    Time: 12 ms (96.79%), Space: 19.6 MB (88.01%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        return majorityElement(nums, 0, nums.size() - 1);
+    }
+private:
+    int majorityElement(const vector<int>& nums, int left, int right) {
+        // base case: no more search space
+        if (left == right) {
+            return nums[left];
+        }
+
+        // divide and conquer:
+        // split nums in half until it cannot be splited
+        int mid = left + (right - left) / 2;
+        int ml = majorityElement(nums, left, mid);
+        int mr = majorityElement(nums, mid + 1, right);
+
+        // return majority element
+        if (ml == mr) {
+            return ml;
+        }
+        auto start = begin(nums) + left;
+        auto end = begin(nums) + right + 1;
+        return count(start, end, ml) > count(start, end, mr) ? ml : mr;
+    }
+};
