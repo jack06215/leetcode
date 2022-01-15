37d5e33672edc7134a35f3322d449d488993740b
Jack Cho <jack06215@users.noreply.github.com>
2022-01-15 14:45:29 +0800

    Time: 0 ms (100.00%), Space: 6.3 MB (14.57%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    vector<int> memo;
+    
+    int _climbStairs(int n) {
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+        memo[n] = _climbStairs(n - 1) + _climbStairs(n - 2);
+        return memo[n];
+    }
+public:
+    int climbStairs(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        memo.resize(n + 1, -1);
+        memo[0] = 1;
+        memo[1] = 1;
+        return _climbStairs(n);
+    }
+};
