3a6770817cda6554adb36b97f707ddd6f8203026
Jack Cho <jack06215@users.noreply.github.com>
2022-01-16 15:51:30 +0800

    Time: 932 ms (5.59%), Space: 247 MB (5.28%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,44 +1,51 @@
 class Solution {
 public:
     void combine(vector<vector<int> >& points, int lo, int mid, int hi) {
-    int lsize = mid - lo + 1, rsize = hi - mid;
-    vector<pair<vector<int>, double> > L(lsize + 1), R(rsize + 1);
-    for(int i = 0; i < lsize; i++)
-        L[i] = make_pair(points[lo + i], sqrt(pow(points[lo + i][1], 2) + pow(points[lo + i][0], 2)));
+        int lsize = mid - lo + 1;
+        int rsize = hi - mid;
+        vector<pair<vector<int>, double>> L(lsize + 1);
+        vector<pair<vector<int>, double>> R(rsize + 1);
+        for (int i = 0; i < lsize; i++) {
+            L[i] = { points[lo + i], sqrt(pow(points[lo + i][1], 2) + pow(points[lo + i][0], 2)) };
+        }
 
-    for(int i = 0; i < rsize; i++)
-        R[i] = make_pair(points[mid + i + 1], sqrt(pow(points[mid + i + 1][1], 2) + pow(points[mid + i + 1][0], 2)));
+        for (int i = 0; i < rsize; i++) {
+            R[i] = { points[mid + i + 1], sqrt(pow(points[mid + i + 1][1], 2) + pow(points[mid + i + 1][0], 2)) };
+        }
 
-    L[lsize] = make_pair(vector<int>{ }, numeric_limits<double>::max());
-    R[rsize] = make_pair(vector<int>{ }, numeric_limits<double>::max());
-    int i = 0, j = 0;
-    for(int k = lo; k <= hi; k++) {
-        if(islessequal(L[i].second, R[j].second))
-            points[k] = L[i].first, i++;
-        else
-            points[k] = R[j].first, j++;
+        L[lsize] = { vector<int>{ }, numeric_limits<double>::max() };
+        R[rsize] = { vector<int>{ }, numeric_limits<double>::max() };
+        int i = 0, j = 0;
+        for (int k = lo; k <= hi; k++) {
+            if (islessequal(L[i].second, R[j].second)) {
+                points[k] = L[i].first, i++;
+            }
+            else {
+                points[k] = R[j].first, j++;
+            }
+        }
     }
-}
 
-void kClosest(vector<vector<int> >& points, int lo, int hi) {
-    if(lo < hi) {
-        int mid = floor((lo + hi) >> 1);
-        kClosest(points, lo, mid);
-        kClosest(points, mid + 1, hi);
-        combine(points, lo, mid, hi);
+    void kClosest(vector<vector<int> >& points, int lo, int hi) {
+        if (lo < hi) {
+            int mid = floor((lo + hi) >> 1);
+            kClosest(points, lo, mid);
+            kClosest(points, mid + 1, hi);
+            combine(points, lo, mid, hi);
+        }
     }
-}
 
-vector<vector<int> > kClosest(vector<vector<int> >& points, int K) {
-    vector<vector<int> > closest;
-    kClosest(points, 0, points.size() - 1);
+    vector<vector<int> > kClosest(vector<vector<int> >& points, int K) {
+        vector<vector<int> > closest;
+        kClosest(points, 0, points.size() - 1);
 
-    int counter = 0;
-    for(auto& point : points) {
-        closest.push_back(point);
-        if(++counter == K) break;
+        int counter = 0;
+        for (auto& point : points) {
+            closest.push_back(point);
+            if (++counter == K) {
+                break;
+            }
+        }
+        return closest;
     }
-	
-    return closest;
-}
 };
