841cd0e4fb7fd9afc771598c1cdf16fd5b81d912
Jack Cho <jack06215@users.noreply.github.com>
2022-01-16 15:11:52 +0800

    Time: 251 ms (5.01%), Space: 70.4 MB (6.68%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,44 +1,36 @@
 class Solution {
+    vector<int> arr;
 public:
-	vector<int> arr;
+    int cross(int left, int right, int mid) {
+        int ans1 = numeric_limits<int>::min();
+        int ans2 = numeric_limits<int>::min();
+        for (int i = mid, sum = 0; i >= left; i--) {
+            sum += arr[i];
+            ans1 = max(ans1, sum);
+        }
+        for (int i = mid + 1, sum = 0; i <= right; i++) {
+            sum += arr[i];
+            ans2 = max(ans2, sum);
+        }
+        return ans1 + ans2;
+    }
 
-	int cross(int left,int right,int mid){
-		//if(left==right) return arr[left];
-		int cur=0;
-		int ans1=INT_MIN;
-		int ans2=INT_MIN;
-		for(int i=mid;i>=left;i--){
-			cur+=arr[i];
-			ans1=max(ans1,cur);
-		}
+    int _maxSubArray(int left, int right) {
+        if (left >= right) {
+            return arr[left];
+        }
 
-		cur=0;
-		for(int i=mid+1;i<=right;i++){
-			cur+=arr[i];
-			ans2=max(ans2,cur);
-		}
+        int mid = (left + right) / 2;
+        int sl = _maxSubArray(left, mid);
+        int sr = _maxSubArray(mid + 1, right);
 
-		return ans1+ans2;
+        int sumCross = cross(left, right, mid);
+        return max(sumCross, max(sl, sr));
+    }
 
-
-	}
-
-	int helper(int left, int right){
-		if(left>=right){
-			return arr[left];
-		}
-		int mid=(left+right)/2;
-
-		int sumLeft=helper(left,mid);
-		int sumRight=helper(mid+1,right);
-		int sumCross=cross(left,right,mid);
-
-		return max(sumCross,max(sumLeft,sumRight));
-	}
-	int maxSubArray(vector<int>& nums) {
-		arr=nums;
-		int n=nums.size();
-		return helper(0,n-1);
-
-	}
+    int maxSubArray(vector<int>& nums) {
+        arr = nums;
+        int n = nums.size();
+        return _maxSubArray(0, n - 1);
+    }
 };
