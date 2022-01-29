b0fb40481691a56c5b7b57e0992952bd5ceb5f03
Jack Cho <jack06215@users.noreply.github.com>
2022-01-29 15:57:48 +0800

    Time: 0 ms (100.00%), Space: 6 MB (42.57%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, int64_t n) {
+        if (n < 0) {
+            n = -n;
+            x = 1 / x;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        if ((n & 1) == 0) {
+            return myPow(x * x, n >> 1);
+        }
+        else {
+            return x * myPow(x * x, n >> 1);
+        }
+    }
+};
