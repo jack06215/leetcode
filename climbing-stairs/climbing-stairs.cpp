17b7468d2fa2a152626c18232eaaeb6f6628a899
Jack Cho <jack06215@users.noreply.github.com>
2022-01-29 15:51:08 +0800

    Time: 4 ms, Memory: 6.2 MB - LeetHub

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -1,25 +1,21 @@
-#include <vector>
-
-using namespace std;
-
 class Solution {
-public:
-    int solve(int n, int* arr) {
-        if (arr[n] != -1) {
-            return arr[n];
-        }
-        arr[n] = solve(n - 1, arr) + solve(n - 2, arr);
-        return arr[n];
-    }
+    vector<int> memo;
     
+    int _climbStairs(int n) {
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+        memo[n] = _climbStairs(n - 1) + _climbStairs(n - 2);
+        return memo[n];
+    }
+public:
     int climbStairs(int n) {
-        if (n == 0 || n == 1) {
+        if (n <= 1) {
             return 1;
         }
         memo.resize(n + 1, -1);
-        memo[0] = 1, memo[1] = 1;
-        return solve(n, &memo[0]);
+        memo[0] = 1;
+        memo[1] = 1;
+        return _climbStairs(n);
     }
-private:
-    vector<int> memo;
 };
