29ecfb6cfa604cfd279385eeb9fd8febee4ac091
Jack Cho <jack06215@users.noreply.github.com>
2022-01-29 15:32:29 +0800

    Time: 3 ms (31.23%), Space: 7.4 MB (5.55%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    vector<int> getRow(int row) {
-
-        if (row == 0) {
+    vector<int> getRow(int rowIndex) {
+        // base Case
+        if (rowIndex == 0) {
             return { 1 };
         }
-        vector<int> temp = getRow(row - 1);
-        const int n = temp.size();
 
-        vector<int> res(n + 1);
-        for (int i = 0; i < n + 1; i++) {
-            // add
-            if (i - 1 >= 0 && i < n) {
-                res[i] = temp[i - 1] + temp[i];
-            }
-            // last element
-            else {
-                res[i] = 1;
-            }
+        vector<int> curRow = { 1 };
+        vector<int> prevRow = getRow(rowIndex - 1);
+
+        // fill the current row based on previous row
+        for (int i = 1; i < rowIndex; i++) {
+            curRow.push_back(prevRow[i - 1] + prevRow[i]);
         }
 
-        return res;
+        // fill the last element of current row
+        curRow.push_back(1);
+        return curRow;
     }
 };
