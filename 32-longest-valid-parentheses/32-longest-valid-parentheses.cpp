c32bb1a20f67df306dcb981c2b747b3e7cf3b310
Jack Cho <jack06215@users.noreply.github.com>
2022-01-30 15:40:14 +0800

    Time: 8 ms (29.27%), Space: 7.8 MB (6.71%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> stk;
+        vector<int> dp(s.size() + 1, 0);
+        int res = 0;
+        for (int i = 1; i <= s.size(); i++) {
+            if (s[i - 1] == '(') {
+                stk.push(i - 1);
+                dp[i] = 0;
+            }
+            else if (s[i - 1] == ')') {
+                if (!stk.empty()) {
+                    dp[i] = 0;
+                }
+                if (!stk.empty() && s[stk.top()] == '(') {
+                    dp[i] = dp[i - 1] + dp[stk.top()] + 1;
+                    stk.pop();
+                }
+            }
+            res = max(res, dp[i]);
+        }
+        return 2 * res;
+    }
+};
