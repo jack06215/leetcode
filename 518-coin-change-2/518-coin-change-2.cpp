3f0b07a2b25a607c2a4ae7bafab56c6e6a43d7f3
Jack Cho <jack06215@users.noreply.github.com>
2022-02-19 10:42:27 +0800

    Time: 52 ms (29.51%), Space: 18.6 MB (31.00%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int change(int sum, vector<int>& coins) {
+        if (sum == 0) {
+            return 1;
+        }
+        if (coins.empty()) {
+            return 0;
+        }
+
+        const int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1));
+
+        // Initialization
+        for (int i = 0; i < sum + 1; i++) {
+            dp[0][i] = 0;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            dp[i][0] = 1;
+        }
+
+        // Unbounded Knapsack
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (coins[i - 1] <= j) {
+                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+                }
+                else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+};
